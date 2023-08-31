c0a381d1f5d7f4bf6386b3a7d1c943ff7a4e808b
de6y <de6y@fresnou.org>
2023-08-31 09:27:19 -0700

    colorPicking

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+    color: "#FFFFFF",
+    textColor: "#000000"
+  });
 });
 
 module.exports = router;
 router.post('/', function(req , res){
   res.render('index', {
-    color: req.body.colorPicker
+    color: req.body.colorPicker,
+    textColor: req.body.textColorPicker
   })
 });
+
+function generateRandomHexCode(){
+  let hexCode = "#"
+  while ( hexCode.length < 7){
+    hexCode += (Math.round( Math.random()*15)).toString(16)
+  }
+  return hexCode
+}
